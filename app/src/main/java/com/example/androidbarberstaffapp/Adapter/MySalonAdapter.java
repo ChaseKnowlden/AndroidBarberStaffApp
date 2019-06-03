a8605cac0266fa125ee63e9d0919de9cad65534f
ChaseK <knwolden887@gmail.com>
2019-06-03 15:23:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/androidbarberstaffapp/Adapter/MySalonAdapter.java
@@ -0,0 +1,87 @@
+package com.example.androidbarberstaffapp.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.androidbarberstaffapp.Interface.IRecyclerItemSelectedListener;
+import com.example.androidbarberstaffapp.Model.Salon;
+import com.example.androidbarberstaffapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySalonAdapter extends RecyclerView.Adapter<MySalonAdapter.MyViewHolder> {
+
+    Context context;
+    List<Salon> salonList;
+    List<CardView> cardViewList;
+    LocalBroadcastManager localBroadcastManager;
+
+    public MySalonAdapter(Context context, List<Salon> salonList) {
+        this.context = context;
+        this.salonList = salonList;
+        cardViewList = new ArrayList<>();
+        localBroadcastManager = LocalBroadcastManager.getInstance(context);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.layout_salon,viewGroup,false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MyViewHolder myViewHolder, int i) {
+        myViewHolder.txt_salon_name.setText(salonList.get(i).getName());
+        myViewHolder.txt_salon_address.setText(salonList.get(i).getAddress());
+        if (!cardViewList.contains(myViewHolder.card_salon))
+            cardViewList.add(myViewHolder.card_salon);
+
+        myViewHolder.setiRecyclerItemSelectedListener(new IRecyclerItemSelectedListener() {
+            @Override
+            public void onItemSelected(View view, int position) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return salonList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView txt_salon_name,txt_salon_address;
+        CardView card_salon;
+
+        public void setiRecyclerItemSelectedListener(IRecyclerItemSelectedListener iRecyclerItemSelectedListener) {
+            this.iRecyclerItemSelectedListener = iRecyclerItemSelectedListener;
+        }
+
+        IRecyclerItemSelectedListener iRecyclerItemSelectedListener;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            card_salon = (CardView)itemView.findViewById(R.id.card_salon);
+            txt_salon_address = (TextView)itemView.findViewById(R.id.txt_salon_address);
+            txt_salon_name = (TextView)itemView.findViewById(R.id.txt_salon_name);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            iRecyclerItemSelectedListener.onItemSelected(v,getAdapterPosition());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidbarberstaffapp/Common/Common.java
@@ -0,0 +1,5 @@
+package com.example.androidbarberstaffapp.Common;
+
+public class Common {
+    public static String state_name="";
+}
